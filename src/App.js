5959a932b15019664cc55fd84e27fd720d189384
Vanshika Choudhary <150908623+vanshikach08@users.noreply.github.com>
2025-05-12 09:12:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+         Developed by{" "}
+        <a target="_blank" href="https://github.com/vanshikach08">
+          Vanshika Choudhary
+        </a>{" "}
+       
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
