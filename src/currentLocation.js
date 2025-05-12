5825ae5408dd85db724990b81465e825cd1c4594
Vanshika Choudhary <150908623+vanshikach08@users.noreply.github.com>
2025-05-12 11:56:39 +0530

    Update currentLocation.js

--- src/currentLocation.js
+++ src/currentLocation.js
@@ -1,32 +1,18 @@
 import React from "react";
 import apiKeys from "./apiKeys";
-import Clock from "react-live-clock";
+// import Clock from "react-live-clock"; ❌ Remove this line
 import Forcast from "./forcast";
 import loader from "./images/WeatherIcons.gif";
 import ReactAnimatedWeather from "react-animated-weather";
+
 const dateBuilder = (d) => {
   let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December",
   ];
   let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+    "Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday",
   ];
 
   let day = days[d.getDay()];
@@ -36,11 +22,25 @@ const dateBuilder = (d) => {
 
   return `${day}, ${date} ${month} ${year}`;
 };
+
+// ✅ Add this custom LiveClock component
+const LiveClock = () => {
+  const [time, setTime] = React.useState(new Date());
+
+  React.useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div className="current-time">{time.toLocaleTimeString()}</div>;
+};
+
 const defaults = {
   color: "white",
   size: 112,
   animate: true,
 };
+
 class Weather extends React.Component {
   state = {
     lat: undefined,
@@ -61,12 +61,10 @@ class Weather extends React.Component {
   componentDidMount() {
     if (navigator.geolocation) {
       this.getPosition()
-        //If user allow location service then will fetch data & send it to get-weather function.
         .then((position) => {
           this.getWeather(position.coords.latitude, position.coords.longitude);
         })
         .catch((err) => {
-          //If user denied location service then standard location weather will le shown on basis of latitude & latitude.
           this.getWeather(28.67, 77.22);
           alert(
             "You have disabled location service. Allow 'This APP' to access your location. Your current location will be used for calculating Real time weather."
@@ -86,21 +84,12 @@ class Weather extends React.Component {
     clearInterval(this.timerID);
   }
 
-  // tick = () => {
-  //   this.getPosition()
-  //   .then((position) => {
-  //     this.getWeather(position.coords.latitude, position.coords.longitude)
-  //   })
-  //   .catch((err) => {
-  //     this.setState({ errorMessage: err.message });
-  //   });
-  // }
-
   getPosition = (options) => {
     return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject, options);
     });
   };
+
   getWeather = async (lat, lon) => {
     const api_call = await fetch(
       `${apiKeys.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKeys.key}`
@@ -115,40 +104,19 @@ class Weather extends React.Component {
       humidity: data.main.humidity,
       main: data.weather[0].main,
       country: data.sys.country,
-      // sunrise: this.getTimeFromUnixTimeStamp(data.sys.sunrise),
-
-      // sunset: this.getTimeFromUnixTimeStamp(data.sys.sunset),
     });
+
     switch (this.state.main) {
-      case "Haze":
-        this.setState({ icon: "CLEAR_DAY" });
-        break;
-      case "Clouds":
-        this.setState({ icon: "CLOUDY" });
-        break;
-      case "Rain":
-        this.setState({ icon: "RAIN" });
-        break;
-      case "Snow":
-        this.setState({ icon: "SNOW" });
-        break;
-      case "Dust":
-        this.setState({ icon: "WIND" });
-        break;
-      case "Drizzle":
-        this.setState({ icon: "SLEET" });
-        break;
-      case "Fog":
-        this.setState({ icon: "FOG" });
-        break;
-      case "Smoke":
-        this.setState({ icon: "FOG" });
-        break;
-      case "Tornado":
-        this.setState({ icon: "WIND" });
-        break;
-      default:
-        this.setState({ icon: "CLEAR_DAY" });
+      case "Haze": this.setState({ icon: "CLEAR_DAY" }); break;
+      case "Clouds": this.setState({ icon: "CLOUDY" }); break;
+      case "Rain": this.setState({ icon: "RAIN" }); break;
+      case "Snow": this.setState({ icon: "SNOW" }); break;
+      case "Dust": this.setState({ icon: "WIND" }); break;
+      case "Drizzle": this.setState({ icon: "SLEET" }); break;
+      case "Fog": this.setState({ icon: "FOG" }); break;
+      case "Smoke": this.setState({ icon: "FOG" }); break;
+      case "Tornado": this.setState({ icon: "WIND" }); break;
+      default: this.setState({ icon: "CLEAR_DAY" });
     }
   };
 
@@ -162,7 +130,6 @@ class Weather extends React.Component {
               <h3>{this.state.country}</h3>
             </div>
             <div className="mb-icon">
-              {" "}
               <ReactAnimatedWeather
                 icon={this.state.icon}
                 color={defaults.color}
@@ -174,17 +141,14 @@ class Weather extends React.Component {
             <div className="date-time">
               <div className="dmy">
                 <div id="txt"></div>
-                <div className="current-time">
-                  <Clock format="HH:mm:ss" interval={1000} ticking={true} />
-                </div>
+                {/* ✅ Use custom LiveClock instead of <Clock /> */}
+                <LiveClock />
                 <div className="current-date">{dateBuilder(new Date())}</div>
               </div>
               <div className="temperature">
                 <p>
                   {this.state.temperatureC}°<span>C</span>
                 </p>
-                {/* <span className="slash">/</span>
-                {this.state.temperatureF} &deg;F */}
               </div>
             </div>
           </div>
@@ -199,8 +163,8 @@ class Weather extends React.Component {
             Detecting your location
           </h3>
           <h3 style={{ color: "white", marginTop: "10px" }}>
-            Your current location wil be displayed on the App <br></br> & used
-            for calculating Real time weather.
+            Your current location will be displayed on the App <br />
+            & used for calculating Real time weather.
           </h3>
         </React.Fragment>
       );
